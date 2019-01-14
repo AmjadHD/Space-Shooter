d4ad4399e079f0bd1bef2106abea88085fd803c6
AmjadHD <amjadhedhili@outlook.com>
2019-01-14 11:40:09 +0100

    Simplify `SpriteSheet.get_image()`

--- spritesheet.py
+++ spritesheet.py
@@ -6,23 +6,17 @@ class SpriteSheet:
         self.spritesheet = pg.image.load(filename).convert()
         self.rect = self.spritesheet.get_rect()
 
-    def get_image(self, colorkey=None, rect=(), size=(0, 0),
+    def get_image(self, colorkey=None, rect=None, size=None,
                   horizontally=False, vertically=False, angle=0):
-        if len(rect) == 4:
-            if isinstance(rect[2], (int, float)) and isinstance(rect[3], (int, float)):
-                image = pg.Surface((rect[2], rect[3]))
-            else:
-                image = pg.Surface(self.rect.size)
+        if rect is not None:
+            image = pg.Surface((rect[2], rect[3]))
             image.blit(self.spritesheet, (0, 0), rect)
         else:
             image = pg.Surface(self.rect.size)
             image.blit(self.spritesheet, (0, 0))
         image.set_colorkey(colorkey)
-        if size != (0, 0):
+        if size is not None:
             image = pg.transform.scale(image, size)
-        image = pg.transform.flip(image, horizontally, vertically)
+        if horizontally or vertically:
+            image = pg.transform.flip(image, horizontally, vertically)
         return pg.transform.rotate(image.copy(), angle)
-
-
-if __name__ == '__main__':
-    pass
